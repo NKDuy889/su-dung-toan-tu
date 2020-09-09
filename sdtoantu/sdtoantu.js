c9153c2c21155dc9cd63172c8196ab8bbb1a79ee
unknown <NKDuy889>
2020-09-09 13:53:58 +0700

    message

---
+++ sdtoantu/sdtoantu.js
@@ -0,0 +1,6 @@
+inputWidht = prompt("Enter your widht")
+inputHeight = prompt("Enter your height")
+let widht = parseInt(inputWidht)
+let height = parseInt(inputHeight)
+let area = widht * height
+document.write("The area is: "+ area)
